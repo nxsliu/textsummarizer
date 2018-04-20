bb26720f1d55ae8eacec18ef2ecc850bc320f385
Nathan Liu <nxsliu@gmail.com>
2018-04-20 18:49:55 +0100

    indexed words

--- prepare-text.py
+++ prepare-text.py
@@ -1,33 +1,30 @@
 from os import listdir
 from os.path import isfile, join
+import pickle
+
 contentfiles = [f for f in listdir('data/bbc-fulltext/bbc/business') if isfile(join('data/bbc-fulltext/bbc/business', f))]
 
 heads = []
+descs = []
 for contentfile in contentfiles:
     with open('data/bbc-fulltext/bbc/business/%s'%contentfile) as f:
-        contentraw = f.readlines();        
-        heads.append(contentraw)
-
-print(len(heads))
-
-
-# with open('data/bbc-fulltext/bbc/business/001.txt') as f:
-#     content = f.readlines()
-
-# content = [x.strip() for x in content]
-# head = content[0]
-# desc = ''.join(content[2:len(content)])
+        contentraw = f.readlines()
+        contentraw = [x.strip() for x in contentraw]
+        contenttokens = [''.join((' {} '.format(el) if el in '[`\=~!@#$%^&*()_+\[\]\{\};\'\\:"|<,./<>?£]' else el for el in c)) for c in contentraw]        
+        heads.append(contenttokens[0])
+        descs.append(''.join(contenttokens[2:len(contenttokens)]))
 
 # print('-----------head tokens-----------')
-# headtokens = ''.join((' {} '.format(el) if el in '[`\-=~!@#$%^&*()_+\[\]\{\};\'\\:"|<,./<>?£]' else el for el in head))
-# print(headtokens)
-
+# print(heads)
 # print('----------- desc tokens -----------')
+# print(descs)
 
-# desctokens = ''.join((' {} '.format(el) if el in '[`\-=~!@#$%^&*()_+\[\]\{\};\'\\:"|<,./<>?£]' else el for el in desc))
-# print(desctokens)
+with open("data/tokens.pkl", "wb") as f:
+    pickle.dump((heads,descs), f)
 
-# print(content[0])  
+# pickle.dump(heads, open('data/tokens.pkl', 'wb'))
+# pickle.dump(descs, open('data/tokens.pkl', 'wb'))
 
-# for x in range(2, len(content)):
-#     print(content[x])
+# print(pickle.load(open('data/tokens.pkl', 'rb')))
+# pickle.load(open('data/tokens.pkl', 'wb'))
+
--- vocabulary-embedding.py
+++ vocabulary-embedding.py
@@ -7,7 +7,7 @@ lower = False
 import pickle as pkl
 FN0 = 'tokens'
 with open('data/%s.pkl'%FN0, 'rb') as fp:
-    heads, desc, keywords = pkl.load(fp)
+    heads, desc = pkl.load(fp)
 
 if lower:
     heads = [h.lower() for h in heads]
@@ -16,4 +16,65 @@ if lower:
     desc = [h.lower() for h in desc]
 
 i=0
-heads[i]
+print(heads[i])
+print(desc[i])
+print(len(heads), len(set(heads)))
+print(len(desc), len(set(desc)))
+
+from collections import Counter
+from itertools import chain
+
+def get_vocab(lst):
+    vocabcount = Counter(w for txt in lst for w in txt.split())
+    vocab = list(map(lambda x: x[0], sorted(vocabcount.items(), key=lambda x: -x[1])))
+    return vocab, vocabcount
+
+vocab, vocabcount = get_vocab(heads+desc)
+
+# print(vocab[:50])
+# print('...',len(vocab))
+
+# import matplotlib.pyplot as plt
+# #%matplotlib inline
+# plt.plot([vocabcount[w] for w in vocab])
+# plt.gca().set_xscale("log", nonposx='clip')
+# plt.gca().set_yscale("log", nonposy='clip')
+# plt.title('word distribution in headlines and discription')
+# plt.xlabel('rank')
+# plt.ylabel('total appearances')
+# plt.show()
+
+empty = 0
+eos = 1
+start_idx = eos + 1
+
+def get_idx(vocab, vocabcount):
+    word2idx = dict((word, idx+start_idx) for idx, word in enumerate(vocab))
+    word2idx['<empty>'] = empty
+    word2idx['<eos>'] = eos
+
+    idx2word = dict((idx, word) for word, idx in word2idx.items())
+
+    return word2idx, idx2word
+
+word2idx, idx2word = get_idx(vocab, vocabcount)
+
+#print(word2idx, idx2word)
+
+fname = 'glove.6B.%dd.txt'%embedding_dim
+import os
+datadir_base = os.path.expanduser(os.path.join('~', '.keras'))
+if not os.access(datadir_base, os.W_OK):
+    datadir_base = os.path.join('/tmp', '.keras')
+
+datadir = os.path.join(datadir_base, 'datasets')
+glove_name = os.path.join(datadir, fname)
+
+if not os.path.exists(glove_name):
+    path = 'glove.6B.zip'
+    path = get_file(path, origin="http://nlp.stanford.edu/data/glove.6B.zip")
+    unzip {datadir}/{path}
+
+glove_n_symbols = !wc -1 {glove_name}
+glove_n_symbols = int(glove_n_symbols[0].split()[0])
+print(glove_n_symbols)
